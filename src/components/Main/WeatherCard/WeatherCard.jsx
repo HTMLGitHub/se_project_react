0be659d86958d16a67fc81716a6d1401db6698f0
Matthew Lee <bak120405@gmail.com>
2024-12-26 13:48:24 -0500

    - add fonts to project - add fonts styles into project - finish weatherCard

--- src/components/Main/WeatherCard/WeatherCard.jsx
+++ src/components/Main/WeatherCard/WeatherCard.jsx
@@ -2,5 +2,8 @@ import './WeatherCard.css';
 
 export default function WeatherCard() {
 return (
-    <div className="weather__card">Cold</div>
+    <div className="weathercard__container">
+        <h3 className="weathercard__tempature">39&deg;F</h3>
+        <img src="" alt="" className="weathercard__image" />
+    </div>
 );}
