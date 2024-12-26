b7744facf7dc9cf3be02493825b4f099339a741e
Matthew Lee <bak120405@gmail.com>
2024-12-26 11:11:37 -0500

    - import images - add folder, and change folder structure - add files to approperate folders - finish Header section

---
+++ src/components/App/Header/Header.jsx
@@ -0,0 +1,25 @@
+import "./Header.css";
+import logo from "../../../assets/logo.svg";
+import avatar from "../../../assets/avatar.png";
+
+export default function Header() {
+
+    const currentDate = new Date().toLocaleString('default', {month: 'long', day: 'numeric'});
+    return (
+        <header className="header">
+            
+            <img src={logo} alt="logo" className="header__logo"></img>
+            <div className="header__datelocation-container">
+                <div className="header__date">{currentDate}</div>
+                <div className="header__location">, London, UK</div>
+            </div>
+            
+            <button className="header__add-clothes-button">+ Add Clothes</button>
+            <div className="header__user-container">
+                <p className="header__user-name">Terrence Tegegne</p>
+                <img src={avatar} alt="Terrence Tegegne" className="header__user-avatar"></img>
+            </div>
+            
+        </header>
+    );
+}
