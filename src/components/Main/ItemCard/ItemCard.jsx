376a6a8cc18cebbecb5b1f3d97ffceeb8778a331
Matthew Lee <bak120405@gmail.com>
2025-01-01 12:36:26 -0500

    - finish styling itemcard

--- src/components/Main/ItemCard/ItemCard.jsx
+++ src/components/Main/ItemCard/ItemCard.jsx
@@ -2,9 +2,9 @@ import "./ItemCard.css";
 
 export default function ItemCard({card}) {
   return (
-    <div className="itemcard__container">
+    <li className="itemcard__container">
       <h2 className="itemcard__name">{card.name}</h2>
       <img className="itemcard__image" src={card.link} alt={card.name} />
-    </div>
+    </li>
   );
 }
