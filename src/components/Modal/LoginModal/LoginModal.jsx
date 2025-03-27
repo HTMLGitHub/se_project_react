2f313cd9a89a4b10ebe760d87f4315775dd297c9
Matthew Lee <bak120405@gmail.com>
2025-03-27 10:43:20 -0400

    - remove unneeded db.json file - update package(-lock).json file - create folder and files for register and login - code register model

---
+++ src/components/Modal/LoginModal/LoginModal.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import ModalWithForm from '../ModalWithForm/ModalWithForm';
+import "../ModalWithForm/ModalWithForm.css";
+import PropTypes from "prop-types";
+
+export default function LoginModal({
+    closeActiveModal,
+    activeModal,
+    onLoginModal,
+    isSaving,
+}) {
+}
---
+++ src/components/Modal/RegisterModal/RegisterModal.jsx
@@ -0,0 +1,115 @@
+import React, {useState, useEffect} from 'react';
+import ModalWithForm from '../ModalWithForm/ModalWithForm';
+import "../ModalWithForm/ModalWithForm.css";
+import PropTypes from "prop-types";
+
+export default function RegisterModal({
+    closeActiveModal, 
+    activeModal,
+    onRegister,
+    isSaving}) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [avatar, setAvatar] = useState("");    
+    //const [isFormValid, setIsFormValid] = useState(false);
+    
+    useEffect(() => {
+        if (activeModal === "register") {
+            setName("");
+            setAvatar("");
+            setEmail("");
+            setPassword("");
+        }
+    }, [activeModal]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onRegister({name, avatar, email, password});
+    }
+
+    return (
+        <ModalWithForm
+            title="Sign Up"
+            buttonText={isSaving ? "Registering..." : "Next"}
+            activeModal={activeModal}
+            closeActiveModal={closeActiveModal}
+            handleSubmit={handleSubmit}
+            isFormValid={true}
+        >
+            <label 
+                htmlFor='email'
+                className='modal__label'
+            >
+                Email*
+                <input
+                    type="email"
+                    className="modal__input modal__input_type_email"
+                    id="email"
+                    placeholder="Email"
+                    required
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}
+                    value={email}
+                />
+            </label>
+            <label 
+                htmlFor='password'
+                className='modal__label'
+            >
+                Password*
+                <input
+                    type="password"
+                    className="modal__input modal__input_type_password"
+                    id="password"
+                    placeholder="Password"
+                    required
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                    value={password}
+                />
+            </label>
+            <label 
+                htmlFor='name'
+                className='modal__label'
+            >
+                Name
+                <input
+                    type="text"
+                    className="modal__input modal__input_type_name"
+                    id="name"
+                    placeholder="Name"
+                    onChange={(e) => {
+                        setName(e.target.value);
+                    }}
+                    value={name}
+                />
+            </label>
+            <label 
+                htmlFor='avatar'
+                className='modal__label'
+            >
+                Avatar
+                <input
+                    type="url"
+                    className="modal__input modal__input_type_avatar"
+                    id="avatar"
+                    placeholder="Avatar URL"
+                    onChange={(e) => {
+                        setAvatar(e.target.value);
+                    }}
+                    value={avatar}
+                />
+            </label>
+        </ModalWithForm>
+    );
+}
+
+RegisterModal.propTypes = {
+    closeActiveModal: PropTypes.func.isRequired,
+    activeModal: PropTypes.string.isRequired,
+    onRegister: PropTypes.func.isRequired,
+    isSaving: PropTypes.bool.isRequired,
+};
