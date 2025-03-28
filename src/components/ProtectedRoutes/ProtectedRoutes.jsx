b4b0eee23cddbeb5342f9a9a940d7edf11cac8d5
Matthew Lee <bak120405@gmail.com>
2025-03-28 08:49:55 -0400

    Add ProtectedRoute component for route protection based on authentication status

---
+++ src/components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -0,0 +1,26 @@
+import {Navigate, useLocation} from 'react-router-dom';
+
+export default function ProtectedRoute({
+    isLoggedIn,
+    children,
+    anonymous = false
+}) {
+    // Invoke the useLocaiton hook to access the value of the 'from' property from its state object.
+    // If the 'from' property is not present, the value of the 'from' property is set to '/'.
+    const location = useLocation();
+    const from = location.state?.from || '/';
+
+    // If the user is logged in, we redirect them away from our anoymous routes.
+    if (isLoggedIn && anonymous) {return <Navigate to={from} />;}
+
+    // If the user is not logged in and tries to access a route taht requires authorization, 
+    // we redirect them to the /login route.
+    if (!isLoggedIn && !anonymous) {
+        // While redirecting to /login, we set the location objects state.from property to the current location value.
+        // This allows us to redirect the user back to the route they were trying to access after they have logged in.
+        return <Navigate to="/login"  state={{from: location}}/>;
+    }
+
+    // Otherwise, display the children components.
+    return children;
+}
